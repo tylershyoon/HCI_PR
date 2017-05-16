146f626cb6968a59b2c4d3121431c7e4c815a007
tylershyoon <tylershyoon@gmail.com>
2017-05-16 23:13:00 +0900

    final commit

--- capital_game.js
+++ capital_game.js
@@ -9,28 +9,53 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-/* rowNum & actionNum retrieval */
-var rowNum; // rowNum saved
-var actionNum; // actionNum saved
+/* rowNum & undoNum retrieval */
+var rowNum, undoNum, redoNum; // rowNum saved
 var qnasRef = database.ref("qnas");
-var actionStack = database.ref("actionStack");
-qnasRef.on("value", function(snapshot){
-    if (snapshot.val() == null){
-        rowNum = 0;
-    }else{
-        rowNum = snapshot.val().length;
-    }
-    console.log("rowNum", rowNum);
-});
-actionStack.on("value", function(snapshot){
-    if (snapshot.val() == null){
-        actionNum = 0;
-    }
-    else{
-        actionNum = snapshot.val().length;
-    }
-    console.log("actionNum", actionNum);
-});
+var undoStack = database.ref("undoStack");
+var redoStack = database.ref("redoStack");
+
+function rowNumUpdate() {
+    qnasRef.on("value", function (snapshot) {
+        if (snapshot.val() == null) {
+            rowNum = 0;
+        }
+        else {
+            rowNum = snapshot.val().length;
+        }
+        console.log("rowNumUpdate", rowNum);
+    });
+}
+function undoUpdate() {
+    undoStack.on("value", function (snapshot) {
+        if (snapshot.val() == null) {
+            document.getElementById("pr3__undo").disabled = true;
+            undoNum = 0;
+        }
+        else {
+            document.getElementById("pr3__undo").disabled = false;
+            undoNum = snapshot.val().length;
+        }
+        console.log("undoNumUpdate", undoNum);
+    });
+}
+function redoUpdate() {
+    redoStack.on("value", function (snapshot) {
+        if (snapshot.val() == null) {
+            document.getElementById("pr3__redo").disabled = true;
+            redoNum = 0;
+        }
+        else {
+            document.getElementById("pr3__redo").disabled = false;
+            redoNum = snapshot.val().length;
+        }
+        console.log("redoUpdate:", redoNum);
+    });
+}
+
+rowNumUpdate();
+undoUpdate();
+redoUpdate();
 
 /* load countries & capitals from the web */
 $( document ).ready(function() {
@@ -60,7 +85,7 @@ function onclickfunc(){
     var rowNum_before_added = rowNum;
     console.log("rN prev", rowNum);
     var qnasRef = database.ref("qnas"); // added declaration
-    var actionStack = database.ref("actionStack");
+    var undoStack = database.ref("undoStack");
     var currentCountry = document.getElementById('pr2__question').innerHTML;
     var userAnswer = document.getElementById('pr2__answer').value;
 
@@ -75,9 +100,9 @@ function onclickfunc(){
 
     tableExpand(currentCountry, userAnswer, iscorrect);
 
-    actionStack.child(actionNum).set({
+    undoStack.child(undoNum).set({
+        undoNum: undoNum,
         action: 'insert',
-        applying: true,
         rowNum: rowNum_before_added
     });
     qnasRef.child(rowNum).set({
@@ -93,6 +118,13 @@ function onclickfunc(){
     document.getElementById('pr2__question').innerHTML = nextCountry;
     document.getElementById('pr2__answer').value = "";
     document.getElementById('pr2__answer').focus();
+
+    var country = nextCountry;
+    if (country == "Côte d'Ivoire"){
+        country == "cote divoire";
+    }
+    document.getElementById('map').setAttribute("src", "https://www.google.com/maps/embed/v1/place?key=AIzaSyDs0oJ1XbVWWj4vayCJMIk4yzUh03e2oPY&q="+country+"&maptype=satellite");
+
 }
 
 function selectFilter(ide){
@@ -112,31 +144,216 @@ function selectFilter(ide){
 }
 
 function selectUndo(){
-    //
+    var redoStack = database.ref('redoStack');
+    var redoStack_del = database.ref('redoStack');
+    console.log("selectUndo begin");
+    var target_undo = undoNum-1;
+    var undoref = database.ref("undoStack/"+target_undo);
+    undoref.once("value", function(snapshot){
+        var undo_content = snapshot.val();
+        console.log("undo_content:", undo_content);
+        if(undo_content.action=="insert"){
+            console.log("undo content action == insert");
+            var target_qna = database.ref("qnas/"+undo_content.rowNum);
+            target_qna.update({isDeleted:true}, function(){
+                $("#"+undo_content.rowNum).hide();
+                if($("#"+undo_content.rowNum).hasClass("correct")){
+                    $("#"+undo_content.rowNum).removeClass('correct');
+                    $("#"+undo_content.rowNum).addClass('correct_deleted');
+                }
+                else if($("#"+undo_content.rowNum).hasClass("wrong")){
+                    $("#"+undo_content.rowNum).removeClass('wrong');
+                    $("#"+undo_content.rowNum).addClass('wrong_deleted');
+                }
+                redoStack.child(redoNum).set({
+                    action: undo_content.action,
+                    rowNum: undo_content.rowNum,
+                    redoNum: redoNum
+                });
+                undoref.remove();
+            });
+        }
+        else if(undo_content.action=="delete"){
+            console.log("undo content action == delete");
+            console.log("undo_content:", undo_content);
+            var target_qna = database.ref("qnas/"+undo_content.rowNum);
+            target_qna.update({isDeleted:false}, function(){
+                $("#"+undo_content.rowNum).show();
+                if($("#"+undo_content.rowNum).hasClass("correct_deleted")){
+                    $("#"+undo_content.rowNum).removeClass('correct_deleted');
+                    $("#"+undo_content.rowNum).addClass('correct');
+                }
+                else if($("#"+undo_content.rowNum).hasClass("wrong_deleted")){
+                    $("#"+undo_content.rowNum).removeClass('wrong_deleted');
+                    $("#"+undo_content.rowNum).addClass('wrong');
+                }
+                redoStack_del.child(redoNum).set({
+                    action: 'delete',
+                    rowNum: undo_content.rowNum,
+                    redoNum: redoNum
+                });
+                undoref.remove();
+            })
+        }
+        else if(undo_content.action=="clear") {
+            console.log("rows", undo_content.rowNum);
+            var target_qnas = undo_content.rowNum;
+            for (var i=0;i<target_qnas.length;i++){
+                var target_qna = database.ref("qnas/"+ target_qnas[i]);
+                target_qna.update({isDeleted:false});
+                $("#"+target_qnas[i]).show();
+                if($("#"+target_qnas[i]).hasClass("correct_deleted")){
+                    $("#"+target_qnas[i]).removeClass('correct_deleted');
+                    $("#"+target_qnas[i]).addClass('correct');
+                }
+                else if($("#"+target_qnas[i]).hasClass("wrong_deleted")){
+                    $("#"+target_qnas[i]).removeClass('wrong_deleted');
+                    $("#"+target_qnas[i]).addClass('wrong');
+                }
+            }
+            redoStack.child(redoNum).set({
+                action: undo_content.action,
+                rowNum: target_qnas,
+                redoNum: redoNum
+            });
+            undoref.remove();
+        }
+    });
 }
 function selectRedo(){
-    //
-}
-function selectClear(){
-    for(var i=0;i<rowNum;i++){
-        var clearRef = database.ref("qnas/" + i);
-        clearRef.update({isDeleted: true});
-    }
-    actionStack.child(actionNum).set({
-        action: 'clear',
-        applying: 'true',
-        rowNum: 'all'
+    console.log("selectRedo begin");
+    var target_redo = redoNum-1;
+    console.log("target_redo:", target_redo);
+    var redoref = database.ref("redoStack/"+target_redo);
+    var redo_content;
+    redoref.once("value").then(function(snapshot){
+        console.log("redoref once begin");
+        redo_content = snapshot.val();
+        if(redo_content.action=="insert"){
+            console.log("redo_content action == insert");
+            var target_qna = database.ref("qnas/"+redo_content.rowNum);
+            target_qna.update({isDeleted:false}, function(){
+                $("#"+redo_content.rowNum).show();
+                if($("#"+redo_content.rowNum).hasClass("correct_deleted")){
+                    $("#"+redo_content.rowNum).removeClass('correct_deleted');
+                    $("#"+redo_content.rowNum).addClass('correct');
+                }
+                else if($("#"+redo_content.rowNum).hasClass("wrong_deleted")){
+                    $("#"+redo_content.rowNum).removeClass('wrong_deleted');
+                    $("#"+redo_content.rowNum).addClass('wrong');
+                }
+                undoStack.child(undoNum).set({
+                    action: redo_content.action,
+                    rowNum: redo_content.rowNum,
+                    undoNum: undoNum
+                });
+                redoref.remove();
+            });
+        }
+        else if(redo_content.action=="delete"){
+            console.log("redo content action == delete");
+            console.log("redo_content under delete:", redo_content);
+            var target_qna = database.ref("qnas/"+redo_content.rowNum);
+            console.log("target_qna: ", target_qna);
+            target_qna.update({isDeleted:true}, function(){
+                $("#"+redo_content.rowNum).hide();
+                if($("#"+redo_content.rowNum).hasClass("correct")){
+                    $("#"+redo_content.rowNum).removeClass('correct');
+                    $("#"+redo_content.rowNum).addClass('correct_deleted');
+                }
+                else if($("#"+redo_content.rowNum).hasClass("wrong")){
+                    $("#"+redo_content.rowNum).removeClass('wrong');
+                    $("#"+redo_content.rowNum).addClass('wrong_deleted');
+                }
+                undoStack.child(undoNum).set({
+                    action: redo_content.action,
+                    rowNum: redo_content.rowNum,
+                    undoNum: undoNum
+                });
+                redoref.remove();
+            });
+        }
+        else if(redo_content.action=="clear"){
+            var target_qnas = redo_content.rowNum;
+            for (var i=0;i<target_qnas.length;i++){
+                var target_qna = database.ref("qnas/"+target_qnas[i]);
+                target_qna.update({isDeleted:true});
+                $("#"+target_qnas[i]).hide();
+                if($("#"+target_qnas[i]).hasClass("correct")){
+                    $("#"+target_qnas[i]).removeClass('correct');
+                    $("#"+target_qnas[i]).addClass('correct_deleted');
+                }
+                else if($("#"+target_qnas[i]).hasClass("wrong")){
+                    $("#"+target_qnas[i]).removeClass('wrong');
+                    $("#"+target_qnas[i]).addClass('wrong_deleted');
+                }
+            }
+            undoStack.child(undoNum).set({
+                action: redo_content.action,
+                rowNum: target_qnas,
+                undoNum: undoNum
+            });
+            redoref.remove();
+        }
     });
 }
 
-function deleteButton(ide){
-    $("#"+ide).remove();
+function undoredoShortcuts(event){
+    if (event.ctrlKey && event.keyCode==90){
+        selectUndo();
+    }
+    else if (event.ctrlKey && event.keyCode==89){
+        selectRedo();
+    }
+}
 
+function selectClear(){
+    var cleared = [];
+    for(var i=0;i<rowNum;i++){
+        var clearRef = database.ref("qnas/" + i);
+        console.log("test", clearRef.isDeleted);
+        clearRef.once("value", function(snapshot){
+            console.log("test2", snapshot.val().isDeleted);
+            if(snapshot.val().isDeleted==false){
+                clearRef.update({isDeleted:true});
+                $("#"+i).hide();
+                if($("#"+i).hasClass("correct")){
+                    $("#"+i).removeClass('correct');
+                    $("#"+i).addClass('correct_deleted');
+                }
+                else if($("#"+i).hasClass("wrong")){
+                    $("#"+i).removeClass('wrong');
+                    $("#"+i).addClass('wrong_deleted');
+                }
+                cleared.push(i);
+            }
+            console.log("cleared:", cleared);
+        });
+    }
+    if (cleared.length!=0) {
+        undoStack.child(undoNum).set({
+            undoNum: undoNum,
+            action: 'clear',
+            rowNum: cleared
+        });
+    }
+}
+
+function deleteButton(ide){
+    $("#"+ide).hide();
+    if($("#"+ide).hasClass("correct")){
+        $("#"+ide).removeClass('correct');
+        $("#"+ide).addClass('correct_deleted');
+    }
+    else if($("#"+ide).hasClass("wrong")){
+        $("#"+ide).removeClass('wrong');
+        $("#"+ide).addClass('wrong_deleted');
+    }
     var deleteRef = database.ref("qnas/"+ ide);
     deleteRef.update({isDeleted:true});
-    actionStack.child(actionNum).set({
+    undoStack.child(undoNum).set({
+        undoNum: undoNum,
         action: 'delete',
-        applying: true,
         rowNum: ide
     });
 }
@@ -163,6 +380,7 @@ function isCorrect(country, usrAnswer){
             }
         }
     }
+    //tableReload();
 }
 
 function tableExpand(currentCountry, userAnswer, iscorrect) {
@@ -185,30 +403,47 @@ function tableExpand(currentCountry, userAnswer, iscorrect) {
         var row = table.insertRow(3);
         row.className = "correct";
         row.id = rowNum.toString();
-        //console.log(row.id);
         var c1 = row.insertCell(0);
+        c1.className = "countryhover";
         var c2 = row.insertCell(1);
         var c3 = row.insertCell(2);
 
-        c1.innerHTML = currentCountry;
+        var country = currentCountry;
+        if (country=="Côte d'Ivoire"){
+            country = "cote divoire";
+        }
+        var url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDs0oJ1XbVWWj4vayCJMIk4yzUh03e2oPY&q=' + country + "&maptype=satellite";
+        var onclick = 'document.getElementById(\'map\').setAttribute(\'src\',\'';
+        var inner = "<div onclick=\"" + onclick + url +"\')" + '\"' + ">" + currentCountry + "</div>";
+        //console.log(inner);
+
+        c1.innerHTML = inner;
         c2.innerHTML = userAnswer;
 
         var button = " <button type='button' onclick='deleteButton(" + row.id + ")'>Delete</button>";
         c3.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i>" + button;
     }
     else{
-        //rowNum += 1;
-        //console.log(rowNum);
         var row = table.insertRow(3);
         row.className = "wrong";
         row.id = rowNum.toString();
-        //console.log(row.id);
         var c1 = row.insertCell(0);
+        c1.className = "countryhover";
         var c2 = row.insertCell(1);
         c2.className = "strikethrough";
         var c3 = row.insertCell(2);
 
-        c1.innerHTML = currentCountry;
+        var country = currentCountry;
+        if (country=="Côte d'Ivoire"){
+            country = "cote divoire";
+        }
+
+        var url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDs0oJ1XbVWWj4vayCJMIk4yzUh03e2oPY&q=' + country + "&maptype=satellite";
+        var onclick = 'document.getElementById(\'map\').setAttribute(\'src\',\'';
+        var inner = "<div onclick=\"" + onclick + url +"\')" + '\"' + ">" + currentCountry + "</div>";
+        //console.log(inner);
+
+        c1.innerHTML = inner;
         c2.innerHTML = userAnswer;
 
         var button = " <button type='button' onclick='deleteButton(" + row.id + ")'>Delete</button>";
@@ -218,24 +453,81 @@ function tableExpand(currentCountry, userAnswer, iscorrect) {
 
 function tableReload(){
     var table = document.getElementById("main__table");
+    console.log("func");
     qnasRef.once("value", function(snapshot){
         snapshot.forEach(function(childSnapshot){
             var childData = childSnapshot.val();
-            console.log(childData.userAnswer);
+            var row = table.insertRow(3);
+            var c1 = row.insertCell(0);
+            c1.className = "countryhover";
+            var c2 = row.insertCell(1);
+            var c3 = row.insertCell(2);
+            if (childData.isCorrect) {
+                row.className = "correct";
+            }
+            else {
+                row.className = "wrong";
+                c2.className = "strikethrough";
+            }
+            row.id = childData.rowNum.toString();
+            var country = childData.currentCountry;
+            if (country=="Côte d'Ivoire"){
+                country = "cote divoire";
+            }
+            var url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDs0oJ1XbVWWj4vayCJMIk4yzUh03e2oPY&q=' + country + "&maptype=satellite";
+            var onclick = 'document.getElementById(\'map\').setAttribute(\'src\',\'';
+            var inner = "<div onclick=\"" + onclick + url +"\')" + '\"' + ">" + childData.currentCountry + "</div>";
+            //console.log(inner);
+            c1.innerHTML = inner;
+            c2.innerHTML = childData.userAnswer;
+
+            var button = " <button type='button' onclick='deleteButton(" + row.id + ")'>Delete</button>";
+            c3.innerHTML = childData.correctAnswer + button;
+
+            if (childData.isDeleted){
+                $("#"+childData.rowNum).hide();
+                if(childData.isCorrect){
+                    $("#"+childData.rowNum).removeClass('correct');
+                    $("#"+childData.rowNum).addClass('correct_deleted');
+                }
+                else{
+                    $("#"+childData.rowNum).removeClass('wrong');
+                    $("#"+childData.rowNum).addClass('wrong_deleted');
+                }
+            }
         })
     });
-    var table = document.getElementById("main__table");
-    if(document.getElementById('filterw').checked){
-        if(iscorrect[0]==true){
-            document.getElementById('filtera').checked = true;
-            selectFilter('filtera');
-        }
-    }
-    else if(document.getElementById('filterc').checked){
-        if(iscorrect[0]==false){
-            document.getElementById('filtera').checked = true;
-            selectFilter('filtera');
-        }
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
